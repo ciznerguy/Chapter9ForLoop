1f4f2fdd5a2118e9f6635e1231986849494872d1
Guy  Cizner <46866327+ciznerguy@users.noreply.github.com>
2024-11-04 21:52:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter9ForLoop
+{
+    internal class Program
+    {
+        // יש לכתוב תוכנית המדפיסה את כל המספרים מ 1 עד 100
+
+        // יש לכתוב תכנית המדפיסה את כל המספרים הזוגיים מ-1 עד 100
+
+        // עמוד 117 תרגיל 11
+
+        // עמוד 117 תרגיל 13
+
+        // עמוד 118 תרגיל 15
+
+        // עמוד 118 תרגיל 17
+
+        // תבנית מונה
+
+        // יש לכתוב תכנית הקולטת 50 מספרים ומונה (סופרת) כמה מהם זןוגיים
+
+
+        //עמוד 124 תרגיל 22
+
+        //עמוד 124 תרגיל 24
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
